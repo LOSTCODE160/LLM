252adfc58d8803202535bb2bb9b8baa5833397a1
LOSTCODE160 <vithalchoudhary4@gmail.com>
2025-07-07 03:51:55 +0530

    changes

--- tokenzation2.py
+++ tokenzation2.py
@@ -4,11 +4,18 @@ class simpletokenizerv1:
         self.str_to_int=vocab
         self. int_to_str={ i:s for s,i in vocab.items()} # create a mapping from integer token to token
     def encode(self,text):
-        preprosseded=re.split(r'([,.:;?_!"()\']|--|\s)',text)
-        preprosseded=[t for t in preprosseded if t.strip()]
-        ids=[self.str_to_int[t] for t in preprosseded]
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+                                
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+        ids=[self.str_to_int[s] for s in preprocessed]
         return ids
     def decode(self,ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+    
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
         
     
         
